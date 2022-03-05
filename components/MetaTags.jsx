eafe1c26a35452c19b0b5cbbabd70011b144cb2c
Ammar Oker <oker.ammar@gmail.com>
2022-03-05 20:41:31 +0300

    x

--- components/MetaTags.jsx
+++ components/MetaTags.jsx
@@ -10,6 +10,28 @@ export default function MetaTags() {
     'Experienced Javascript Developer with a demonstrated history of working in the internet industry. Skilled in React.js, Express.js, and Full-Stack Development.';
   return (
     <>
+      {/* OG tags */}
+      <meta property="og:type" content="article" />
+      <meta property="og:url" content={hostUrl} />
+      <meta property="og:title" content="Ammar Oker - Software Engineer" />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content="Ammar Oker - Software Engineer" />
+      <meta property="og:image" content={ogImage} />
+      {/* #OG tags */}
+      {/* Twitter tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Ammar Oker - Software Engineer" />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:site" content="@ammar_oker" />
+      <meta name="twitter:creator" content="@ammar_oker" />
+      <meta name="twitter:image" content={twitterImage} />
+      {/* #Twitter tags */}
+      <meta name="description" content={description} />
+      <meta name="author" content="Ammar Oker" />
+      <link
+        itemProp="keywords"
+        href="web developer portfolio, nodejs developer, node.js developer, node js developer, fullstack developer, node developer, vue developer, nodejs developer, web developer, frontend developer portfolio, developer profile, react developer, react native developer, javascript developer"
+      />
       <meta name="theme-color" content="#003B73" />
       {/* favicons */}
       <link
@@ -108,28 +130,6 @@ export default function MetaTags() {
       />
       <meta name="msapplication-config" content="/browserconfig.xml" />
       {/* #favicons */}
-      <link
-        itemProp="keywords"
-        href="web developer portfolio, nodejs developer, node.js developer, node js developer, fullstack developer, node developer, vue developer, nodejs developer, web developer, frontend developer portfolio, developer profile, react developer, react native developer, javascript developer"
-      />
-      {/* OG tags */}
-      <meta name="description" content={description} />
-      <meta name="author" content="Ammar Oker" />
-      <meta property="og:type" content="article" />
-      <meta property="og:url" content={hostUrl} />
-      <meta property="og:title" content="Ammar Oker - Software Engineer" />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content="Ammar Oker - Software Engineer" />
-      <meta property="og:image" content={ogImage} />
-      {/* #OG tags */}
-      {/* Twitter tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Ammar Oker - Software Engineer" />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:site" content="@ammar_oker" />
-      <meta name="twitter:creator" content="@ammar_oker" />
-      <meta name="twitter:image" content={twitterImage} />
-      {/* #Twitter tags */}
       <meta
         name="google-site-verification"
         content="vVippowe29p6icLWSSRXsHMsJLGYRs0hx6hX-0wQSz4"
