9caedf69f11f179aeb2da731e8dbcb6c327d6ae0
Ammar Oker <oker.ammar@gmail.com>
2022-03-05 20:10:25 +0300

    fix icons urls

--- components/MetaTags.jsx
+++ components/MetaTags.jsx
@@ -11,104 +11,107 @@ export default function MetaTags() {
   return (
     <>
       <meta name="theme-color" content="#003B73" />
-
       {/* favicons */}
-      <link rel="shortcut icon" href="/resume/icons/favicon.ico" />
+      <link
+        rel="shortcut icon"
+        href={`${hostUrl}/resume/icons/favicon.ico"`}
+      />{' '}
       <link
         rel="icon"
         sizes="16x16 32x32 64x64"
-        href="/resume/icons/favicon.ico"
+        href={`${hostUrl}/resume/icons/favicon.ico`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="196x196"
-        href="/resume/icons/favicon-192.png"
+        href={`${hostUrl}/resume/icons/favicon-192.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="160x160"
-        href="/resume/icons/favicon-160.png"
+        href={`${hostUrl}/resume/icons/favicon-160.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/resume/icons/favicon-96.png"
+        href={`${hostUrl}/resume/icons/favicon-96.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="64x64"
-        href="/resume/icons/favicon-64.png"
+        href={`${hostUrl}/resume/icons/favicon-64.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/resume/icons/favicon-32.png"
+        href={`${hostUrl}/resume/icons/favicon-32.png`}
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/resume/icons/favicon-16.png"
+        href={`${hostUrl}/resume/icons/favicon-16.png`}
       />
-      <link rel="apple-touch-icon" href="/resume/icons/favicon-57.png" />
+      <link
+        rel="apple-touch-icon"
+        href={`${hostUrl}/resume/icons/favicon-57.png`}
+      />
       <link
         rel="apple-touch-icon"
         sizes="114x114"
-        href="/resume/icons/favicon-114.png"
+        href={`${hostUrl}/resume/icons/favicon-114.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="72x72"
-        href="/resume/icons/favicon-72.png"
+        href={`${hostUrl}/resume/icons/favicon-72.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="144x144"
-        href="/resume/icons/favicon-144.png"
+        href={`${hostUrl}/resume/icons/favicon-144.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="60x60"
-        href="/resume/icons/favicon-60.png"
+        href={`${hostUrl}/resume/icons/favicon-60.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="120x120"
-        href="/resume/icons/favicon-120.png"
+        href={`${hostUrl}/resume/icons/favicon-120.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="76x76"
-        href="/resume/icons/favicon-76.png"
+        href={`${hostUrl}/resume/icons/favicon-76.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="152x152"
-        href="/resume/icons/favicon-152.png"
+        href={`${hostUrl}/resume/icons/favicon-152.png`}
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/resume/icons/favicon-180.png"
+        href={`${hostUrl}/resume/icons/favicon-180.png`}
       />
       <meta name="msapplication-TileColor" content="#003B73" />
       <meta
         name="msapplication-TileImage"
-        content="/resume/icons/favicon-144.png"
+        content={`${hostUrl}/resume/icons/favicon-144.png`}
       />
       <meta name="msapplication-config" content="/browserconfig.xml" />
       {/* #favicons */}
-
       <link
         itemProp="keywords"
         href="web developer portfolio, nodejs developer, node.js developer, node js developer, fullstack developer, node developer, vue developer, nodejs developer, web developer, frontend developer portfolio, developer profile, react developer, react native developer, javascript developer"
       />
-
       {/* OG tags */}
       <meta name="description" content={description} />
       <meta property="og:type" content="article" />
@@ -118,7 +121,6 @@ export default function MetaTags() {
       <meta property="og:site_name" content="Ammar Oker - Software Engineer" />
       <meta property="og:image" content={ogImage} />
       {/* #OG tags */}
-
       {/* Twitter tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="Ammar Oker - Software Engineer" />
@@ -127,7 +129,6 @@ export default function MetaTags() {
       <meta name="twitter:creator" content="@ammar_oker" />
       <meta name="twitter:image" content={twitterImage} />
       {/* #Twitter tags */}
-
       <meta
         name="google-site-verification"
         content="vVippowe29p6icLWSSRXsHMsJLGYRs0hx6hX-0wQSz4"
