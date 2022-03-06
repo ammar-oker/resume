f7fb00fa280c3f390727361afa2dfc871aa0ec17
Ammar Oker <oker.ammar@gmail.com>
2022-03-06 10:13:21 +0300

    x

--- components/MetaTags.jsx
+++ components/MetaTags.jsx
@@ -16,7 +16,7 @@ export default function MetaTags() {
       <meta property="og:title" content="Ammar Oker - Software Engineer" />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content="Ammar Oker - Software Engineer" />
-      <meta property="og:image:url" content={ogImage} />
+      <meta property="og:image" content={ogImage} />
       <meta property="og:image:type" content="image/webp" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
